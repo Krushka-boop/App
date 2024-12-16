47b7ae8b46af1527b5b8b0f94706f0e8deca7eba
LIZA <LIZA@DESKTOP-BP9E8PF>
2024-12-16 21:45:04 +0300

    Изменение в Hel2

--- App/main.cpp
+++ App/main.cpp
@@ -14,7 +14,7 @@ int main()
 	cout << PloshKvadrat(c) << endl;
 	cout << PloshTrangle(a, b) << endl;
 	cout << "Hello, i'm a Hel\n";
-	cout << "This is a not a joke!!!\n";
+	cout << "This is a not a Fray\n";
 	cout << PerimetrCircle(b) << endl;
 	cout << PerimetrKvadrat(a) << endl;
 	cout << PerimetrTrangle(b, c, a) << endl;
