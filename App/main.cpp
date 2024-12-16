41304d5eea34006c652de0d93107d4af938630ef
LIZA <LIZA@DESKTOP-BP9E8PF>
2024-12-16 19:38:39 +0300

    Итоггггггггг

--- App/main.cpp
+++ App/main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
 	double a = 7;
 	double b = 3;
 	double c = 9;
@@ -16,15 +15,5 @@ int main()
 	cout << PerimetrCircle(b) << endl;
 	cout << PerimetrKvadrat(a) << endl;
 	cout << PerimetrTrangle(b,c,a) << endl;
-=======
-	double a = 7;
-	double b = 3;
-	double c = 9;
-	cout << PloshCircle(a) << endl;
-	cout << PloshKvadrat(c) << endl;
-	cout << PloshTrangle(a, c) << endl;
-	cout << PerimetrCircle(b) << endl;
-	cout << PerimetrKvadrat(a) << endl;
-	cout << PerimetrTrangle(b,c,a) << endl;
->>>>>>> Err
+
 }
