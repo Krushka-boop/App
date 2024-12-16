450698ad731ff7067166c6295044ed85171e750f
LIZA <LIZA@DESKTOP-BP9E8PF>
2024-12-16 19:53:13 +0300

    изменения параметров функции

--- App/main.cpp
+++ App/main.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int main()
 {
-	double a = 7;
-	double b = 3;
-	double c = 9;
+	double a = 4;
+	double b = 8;
+	double c = 7;
 	cout << PloshCircle(a) << endl;
 	cout << PloshKvadrat(c) << endl;
-	cout << PloshTrangle(a, c) << endl;
+	cout << PloshTrangle(a, b) << endl;
 	cout << PerimetrCircle(b) << endl;
 	cout << PerimetrKvadrat(a) << endl;
 	cout << PerimetrTrangle(b,c,a) << endl;
